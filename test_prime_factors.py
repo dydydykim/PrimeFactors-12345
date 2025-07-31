b5a3cd0d6b6773082c311e84b0d86f4884e69fba
dykim <dykim4225@gmail.com>
2025-07-31 11:02:21 +0900

    [feature] page 27
    Signed-off-by: dykim <dykim4225@gmail.com>

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,4 +1,8 @@
 import pytest
 
-def test_prime_factors():
-    assert 1 == 1
+from prime_factors import PrimeFactor
+
+
+def test_prime_factor_of_1():
+    prime_factor = PrimeFactor()
+    assert prime_factor.of(1) == []
